d1eff923285647e1fb07eac9907c8ddbed61b974
vivekkumar1994 <96063498+vivekkumar1994@users.noreply.github.com>
2022-11-30 00:13:48 +0530

    create api

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const cors = require("cors");
+
+const app=express();
+app.use(cors());
+const port = process.env.Port || 3000;
+
+const apiData = require("./data.json")
+
+
+require('dotenv').config();
+
+app.get("/",(req,res) =>{
+    res.send(apiData);
+
+});
+
+app.listen(port,() => {
+    console.log(`server running ${port}`);
+})
