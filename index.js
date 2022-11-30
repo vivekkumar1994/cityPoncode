23200a5a0c4bbecabf7c44ac6888c510f7d3b10c
vivekkumar1994 <96063498+vivekkumar1994@users.noreply.github.com>
2022-11-30 08:10:42 +0530

    comment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const app=express();
 app.use(cors());
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 const apiData = require("./data.json");
 
